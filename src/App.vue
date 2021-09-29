dd7ba3f7d767dee71dd5d62483426e8f5acbae3b
anderrf <anderson_rocha_farias@outlook.com>
2021-09-28 22:09:42 -0300

    Componentização da ToolBar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar app color="red" dark>
-        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-toolbar-title>⚽ Campeonato Brasileiro ⚽</v-toolbar-title>
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" absolute temporary>
-        <v-list nav dense>
-          <v-list-item-group v-model="group" active-class="red--text text--lighten-2">
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-soccer</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Início</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-trophy</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Classificação</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
+      <AppToolBar/>
       <Inicio/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+  import AppToolBar from './components/shared/AppToolBar';
   import Inicio from './components/Inicio';
   export default {
     name: 'App',
     components: {
+      AppToolBar,
       Inicio
-    },
-    data() {
-      return {
-        drawer: false,
-        group: null,
-      }
     }
-  };
+  }
 </script>
 
 <style>
